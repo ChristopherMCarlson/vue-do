5ae3d9edf07f9934ec9c5050efd50b06e3e033d4
ChristopherMCarlson <c.m.carlson13@gmail.com>
2020-08-18 22:48:36 -0600

    protected routes working!

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 import Auth from '../views/Auth.vue'
 
 Vue.use(VueRouter)
@@ -9,25 +10,43 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
-    path: '/authorization',
+    path: '/auth',
     name: 'Auth',
     component: Auth
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About,
+    meta: { requiresAuth: true}
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeResolve((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    let user;
+    Vue.prototype.$Amplify.Auth.currentAuthenticatedUser().then(data => {
+      if (data && data.signInUserSession) {
+        user = data;
+      }
+      next()
+    }).catch((e) => {
+      next({
+        path: '/auth',
+        query: {
+          redirect: to.fullPath,
+        }
+      });
+    });
+  }
+  next()
 })
 
 export default router
