62df9543d502f8f4f4aaa6b6bcaf0b0700102757
ChristopherMCarlson <c.m.carlson13@gmail.com>
2020-08-22 02:27:26 -0600

    Its cenetered. Good enough for tonight

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,39 +1,43 @@
 <template>
-  <div id="auth">
-    <form>
-      <v-text-field
-        v-model="username"
-        :error-messages="nameErrors"
-        :counter="10"
-        label="Username"
-        required
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
+    <v-card
+    style="width: 25vw; margin-left: 37.5vw"
+    outlined
+    >
+    <div id="auth">
+      <form>
+        <v-text-field
+          v-model="username"
+          :error-messages="nameErrors"
+          :counter="10"
+          label="Username"
+          required
+          @input="$v.name.$touch()"
+          @blur="$v.name.$touch()"
+        ></v-text-field>
+        <v-text-field
+          v-model="email"
+          :error-messages="emailErrors"
+          label="E-mail"
+          required
+          @input="$v.email.$touch()"
+          @blur="$v.email.$touch()"
+        ></v-text-field>
+        <v-text-field
+        v-model="password"
+        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+        :rules="[rules.required, rules.min]"
+        :type="show1 ? 'text' : 'password'"
+        name="input-10-1"
+        label="Normal with hint text"
+        hint="At least 8 characters"
+        counter
+        @click:append="show1 = !show1"
       ></v-text-field>
-      <v-text-field
-        v-model="email"
-        :error-messages="emailErrors"
-        label="E-mail"
-        required
-        @input="$v.email.$touch()"
-        @blur="$v.email.$touch()"
-      ></v-text-field>
-      <v-text-field
-      v-model="password"
-      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-      :rules="[rules.required, rules.min]"
-      :type="show1 ? 'text' : 'password'"
-      name="input-10-1"
-      label="Normal with hint text"
-      hint="At least 8 characters"
-      counter
-      @click:append="show1 = !show1"
-    ></v-text-field>
-  
-      <v-btn class="mr-4" @click="submit">submit</v-btn>
-      <v-btn @click="clear">clear</v-btn>
-    </form>
-  </div>
+      <v-btn class="mr-4" @click="submit">Register</v-btn>
+      <p>Already have an account? <a @click="test" class="white--text">Click here to login.</a></p>
+      </form>
+    </div>
+  </v-card>
 </template>
 
 <script>
@@ -107,6 +111,9 @@ methods: {
     this.email = ''
     this.password = ''
   },
+  test () {
+    console.log("test clicked");
+  }
 },
 }
 </script>
