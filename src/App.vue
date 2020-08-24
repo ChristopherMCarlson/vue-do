59d77cac95b32f7c46b11ef8bb548627c354f1cf
ChristopherMCarlson <c.m.carlson13@gmail.com>
2020-08-23 21:58:57 -0600

    EOD

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,6 @@
         <v-spacer></v-spacer>
   
         <div v-if='signedIn'>
-          <v-btn icon>
-            <v-icon>mdi-heart</v-icon>
-          </v-btn>
     
           <v-tooltip bottom>
               <template v-slot:activator="{ on, attrs }">
@@ -33,31 +30,7 @@
               </template>
               <span>Tooltip</span>
             </v-tooltip>
-    
-          <v-menu
-            left
-            bottom
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                icon
-                v-bind="attrs"
-                v-on="on"
-              >
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
-            </template>
-    
-            <v-list>
-              <v-list-item
-                v-for="n in 5"
-                :key="n"
-                @click="() => {}"
-              >
-                <v-list-item-title>Option {{ n }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
+            
         </div>
       </v-app-bar>
       <v-row>
